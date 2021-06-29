018731c635a46872d2bc78ebaec2aebb0ba70244
ersati <piotrp92@gmail.com>
2021-06-29 02:01:53 +0100

    add variable size in MB

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ input.addEventListener('change', () => {
   const file = input.files[0];
   const fileName = file.name;
   const fileSizeByte = file.size;
+  const sizeInMB = (fileSizeByte / (1024 * 1024)).toFixed(2);
   const dot = fileString.lastIndexOf('.');
   const slash = fileString.lastIndexOf('\\');
   const format = fileString.slice(dot, fileString.length);
   const name = fileString.slice(slash, dot);
 
-  console.log(input.files[0].type);
+  console.log(input.files[0].type, sizeInMB);
 });
