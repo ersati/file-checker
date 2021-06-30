b71408706a7e5c9d3371b7a871251426a13308f2
ersati <piotrp92@gmail.com>
2021-06-30 01:50:43 +0100

    add function to print name of file size and format, add another button to reset everything

--- index.js
+++ index.js
@@ -2,20 +2,38 @@
 import './style.css';
 
 const input = document.querySelector('input[type=file]');
+const div = document.createElement('div');
+const firstP = document.createElement('p');
+const secondP = document.createElement('p');
+const thirdP = document.createElement('p');
+const body = document.querySelector('body');
+const btn = document.querySelector('.button');
 
-console.log(input);
+div.setAttribute('class', 'last');
+body.appendChild(div);
+div.appendChild(firstP, secondP, thirdP);
+div.appendChild(secondP);
+div.appendChild(thirdP);
 
 input.addEventListener('change', () => {
   const fileString = input.value;
-
   const file = input.files[0];
   const fileName = file.name;
   const fileSizeByte = file.size;
   const sizeInMB = (fileSizeByte / (1024 * 1024)).toFixed(2);
   const dot = fileString.lastIndexOf('.');
-  const slash = fileString.lastIndexOf('\\');
   const format = fileString.slice(dot, fileString.length);
-  const name = fileString.slice(slash, dot);
+  // const slash = fileString.lastIndexOf('\\');
+  // const name = fileString.slice(slash, dot);
 
-  console.log(input.files[0].type, sizeInMB);
+  firstP.textContent = `Name of File: ${fileName}`;
+  secondP.textContent = `Size of File ${sizeInMB}MB`;
+  thirdP.textContent = `Format of File: ${format}`;
+});
+
+btn.addEventListener('click', () => {
+  input.value = null;
+  firstP.textContent = ``;
+  secondP.textContent = ``;
+  thirdP.textContent = ``;
 });
